0b0a2034594f1581a3922c87d0a5ba729c2cc897
Ankit Gupta <ankitmanojg@gmail.com>
2025-08-18 15:53:32 +0530

    11: project-header

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -6,13 +6,16 @@ import {
     ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import { MessagesContainer } from "../components/messages-container";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { Fragment } from "@/generated/prisma";
+import { ProjectHeader } from "../components/project-header";
 
 
 interface Props {
     projectId: string;
 };
 export const ProjectView = ({ projectId }: Props) => {   
+    const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
     return (
         <div className="h-screen">
             <ResizablePanelGroup direction="horizontal">
@@ -20,9 +23,16 @@ export const ProjectView = ({ projectId }: Props) => {
                     defaultSize={35}
                     minSize={20}
                     className="flex flex-col min-h-0"
-                >
-                    <Suspense fallback={<p>Loading messages...</p>}>
-                        <MessagesContainer projectId={projectId} />
+                >   
+                    <Suspense fallback={<p>Loading project...</p>}>
+                        <ProjectHeader projectId={projectId} />
+                    </Suspense> 
+                    <Suspense fallback={<p>Loading messages...</p>}>  
+                        <MessagesContainer 
+                            projectId={projectId}
+                            activeFragment={activeFragment}
+                            setActiveFragment={setActiveFragment} 
+                        />
                     </Suspense>
                 </ResizablePanel>
                 <ResizableHandle withHandle />
